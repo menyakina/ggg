80cd04bd8adc16d0c74ffa352d40533bdf44b420
menyakina <menyakinaz@mail.ru>
2022-12-04 13:19:19 +0300

    добавили функции insert_staff(self), update_twstaffs(self, query="select * from stuff")

--- main.py
+++ main.py
@@ -31,6 +31,35 @@ class MyWidget(QWidget, Ui_Form):
                 self.twStuffs.setItem(i, j, QTableWidgetItem(str(elem)))
         self.twStuffs.resizeColumnsToContents()
 
+    def insert_staff(self):
+        row = [self.leFio.text(), 'муж' if self.rbMale.isChecked() else 'жен', self.sbAge.text(),
+               self.lePhone.text(), self.leEmail.text(), self.cbPost.itemText(self.cbPost.currentIndex()),
+               self.sbExp.text()]
+        try:
+            cur = self.conn.cursor()
+            cur.execute(f"""insert into stuff(fio, sex, age, phone, email, position, exp)
+                values('{row[0]}', '{row[1]}', {row[2]}, '{row[3]}', '{row[4]}', '{row[5]}', {row[6]})""")
+            self.conn.commit()
+            cur.close()
+        except Exception as e:
+            print(f"Исключение: {e}")
+            return e
+        self.update_twStuffs()
+
+    def update_twstaffs(self, query="select * from stuff"):
+        try:
+            cur = self.conn.cursor()
+            data = cur.execute(query).fetchall()
+        except Exception as e:
+            print(f"Проблемы с подключением к БД. {e}")
+            return e
+        self.twStuffs.setRowCount(0)
+        for i, row in enumerate(data):
+            self.twStuffs.setRowCount(self.twStuffs.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.twStuffs.setItem(i, j, QTableWidgetItem(str(elem)))
+        self.twStuffs.resizeColumnsToContents()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
